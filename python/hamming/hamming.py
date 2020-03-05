b1aa1cf98c01b091323ed12e625fd53775dab7f5
Shadd Anderson <shaddanderson18@gmail.com>
2020-03-05 14:21:17 -0600

    updated hamming and added palindrome products side project

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,9 +1,5 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands must be of the same length")
-    else:
-        total = 0
-        for index, letter in enumerate(strand_a):
-            if strand_b[index] != letter:
-                total += 1
-        return total
+
+    return sum([1 for x, y in zip(strand_a, strand_b) if x != y])
